d07bdaed9c790c8bd75cf44fc905232389af9d7c
Dominic Dang <dominicdang@protonmail.com>
2024-06-22 10:30:54 -0500

    fixing darwin run script

--- cpp/raytracing_one_weekend/all-nix.sh
+++ cpp/raytracing_one_weekend/all-nix.sh
@@ -2,7 +2,14 @@
 
 echo ">>> Sourcing shell.nix, building"
 nix-shell --run ./build.sh
-# ./run.sh
 
 ./run.sh >./build/image.ppm
-feh ./build/image.ppm
+case $(uname) in
+Darwin*)
+	open ./build/image.ppm
+	;;
+Linux-*)
+	feh ./build/image.ppm
+	;;
+*) ;;
+esac
