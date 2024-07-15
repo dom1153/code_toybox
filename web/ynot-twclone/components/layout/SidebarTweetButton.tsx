f0ea85abcc825e66aabe2ebe7f57d84b25d1be25
Dominic Dang <dominicdang@protonmail.com>
2024-07-15 18:09:16 -0500

    updated some misc

--- web/ynot-twclone/components/layout/SidebarTweetButton.tsx
+++ web/ynot-twclone/components/layout/SidebarTweetButton.tsx
@@ -6,7 +6,6 @@ const SidebarTweetButton = () => {
 	const router = useRouter();
 
 	return (
-		// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
 		<div onClick={() => router.push("/")}>
 			<div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
 				<FaFeather size={24} color="white" />
