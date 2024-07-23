bca2cab61d520ad626b002e8cd87b73a37887a3c
Dominic Dang <dominicdang@protonmail.com>
2024-07-23 14:57:07 -0500

    some auth ui

--- web/ynot-twclone/pages/_app.tsx
+++ web/ynot-twclone/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
+import LoginModal from "@/components/modals/LoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<LoginModal />
+			{/* <Modal isOpen title="Test Modal" actionLabel="Submit" /> */}
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
