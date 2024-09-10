963d4aa4b8ba10f6f4b2a9769fca6e03477fe147
Dominic Dang <dominicdang@protonmail.com>
2024-09-10 19:11:41 +0000

    return grid to the old style

--- web/nextjs/fictional-spoon/components/myui/dock-archives/shipcard-gallery.tsx
+++ web/nextjs/fictional-spoon/components/myui/dock-archives/shipcard-gallery.tsx
@@ -2,7 +2,6 @@ import { Ship } from "@azurapi/azurapi/build/types/ship"
 
 import { isDevEnv } from "@/lib/myutils"
 import { Card } from "@/components/ui/card"
-import { ScrollArea } from "@/components/ui/scroll-area"
 
 import ShipCard from "../general/shipcard"
 
@@ -13,17 +12,19 @@ interface ShipCardGalleryProps {
 const ShipCardGallery: React.FC<ShipCardGalleryProps> = ({ shipList }) => {
   return (
     <>
-      <Card className="flex-1 overflow-hidden px-2 py-4">
+      <Card className="flex-1 px-2 py-4">
         {/* Chakra SimpleGrid suggests auto-fit with minmax */}
         {/* FYI: fixed ShipCard (e.g. w-40) with breaks card gallery, so leave it dynamic*/}
+        {/* Auto fit VVV ; OK, but bad when there's only 1 or 2 cards (e.g. search) */}
         {/* Variable gaps?, fixed card size minmax(128px, 128px) */}
         {/* Variable cardSize, fixed gaps minmax(128px, 1fr) */}
         {/* Grid gap and any padding will determine flucatation between sizes */}
         {true && (
           <div
-            className="grid justify-evenly gap-x-2 gap-y-4"
+            className="grid justify-evenly gap-x-2 gap-y-4 start"
             style={{
-              gridTemplateColumns: "repeat(auto-fit, minmax(110px, 110px))",
+              gridTemplateColumns: "repeat(auto-fill, 110px)",
+              // gridTemplateColumns: "repeat(auto-fit, minmax(110px, 110px))",
             }}
           >
             {shipList.length > 0 &&
